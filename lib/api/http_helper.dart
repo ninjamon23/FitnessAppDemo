035de074c3a964797cc0d49c83cf5fb76cb6d9dd
Jordan Capoquian <jcapoquian@doehle-mnl.com>
2022-06-21 18:15:31 +0800

    In progress weather app page.

--- lib/api/http_helper.dart
+++ lib/api/http_helper.dart
@@ -1,5 +1,20 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class HttpHelper{
   // https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=183a1ac36d3e05b3d528f4f81bd5d8b9
+  final String authority = 'api.openweathermap.org';
+  final String path = 'data/2.5/weather';
+  final String apiKey = '183a1ac36d3e05b3d528f4f81bd5d8b9';
+
+  Future<String> getWeather(String location) async {
+    Map<String, dynamic> parameters = {
+      'q': location,
+      'appId':apiKey
+    };
+
+    var uri = Uri.https(authority, path, parameters);
+    var result = await http.get(uri);
+
+    return result.body;
+  }
 }
--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,23 +1,38 @@
 import 'package:familiarization/shared/menu_drawer.dart';
 import 'package:flutter/material.dart';
+import '../api/http_helper.dart';
 
+import '../shared/bottom_nav.dart';
 
-
-import '../shared/bottom_nav.dart';class WeatherPage extends StatefulWidget {
-  const WeatherPage({ Key? key }) : super(key: key);
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
 
   @override
   State<WeatherPage> createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  String result = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Weather')),
       drawer: const MenuDrawer(),
       bottomNavigationBar: const BottomNav(),
-      body: const Center(child: FlutterLogo()),
+      body: Column(children: [
+        ElevatedButton(
+            onPressed: getWeatherByLocation, child: const Text('Get Data')),
+        Text(result)
+      ]),
     );
+  }
+
+  Future getWeatherByLocation() async {
+    var helper = HttpHelper();
+    result = await helper.getWeather('London');
+    setState(() {
+      
+    });
   }
 }
